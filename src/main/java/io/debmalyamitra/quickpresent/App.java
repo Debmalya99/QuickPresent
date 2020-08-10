cb56fc8c25f09c6bc78f0727cdd4723296ac0053
Debmalya99 <debmalyamitra99@gmail.com>
2020-08-10 15:25:21 +0530

    First Commit

---
+++ src/main/java/io/debmalyamitra/quickpresent/App.java
@@ -0,0 +1,14 @@
+package io.debmalyamitra.quickpresent;
+
+import javax.swing.*;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        JFrame f = new JFrame("QuickPresent - Make Business Presentations quickly");
+        f.setSize(800,600);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
